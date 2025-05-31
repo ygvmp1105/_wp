bd8462393a2645d8a35e95c54b8fe80bbc0ec34f
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:09:08 +0800

    Update 06.js

--- homework/03/06.js
+++ homework/03/06.js
@@ -2,3 +2,12 @@
 
 定義一個遞迴函數 factorial(n)，計算 n 的階乘（n!）。
 例如 factorial(5) 應該回傳 120。 */
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+console.log(factorial(5));
