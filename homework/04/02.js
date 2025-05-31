7474e638cff17d7905241cd735fa6955e6552b84
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:25:56 +0800

    Update 02.js

--- homework/04/02.js
+++ homework/04/02.js
@@ -1,1 +1,14 @@
-/* */
+/* 陣列去重並排序
+實作 uniqueSorted(arr)，移除陣列重複的元素並從小到大排序。
+
+console.log(uniqueSorted([5, 3, 8, 3, 1, 5, 8])); 
+// [1, 3, 5, 8] */
+function uniqueSorted(arr) {
+    let uniqueArr = [...new Set(arr)];
+
+    uniqueArr.sort((a, b) => a - b);
+
+    return uniqueArr;
+}
+
+console.log(uniqueSorted([5, 3, 8, 3, 1, 5, 8])); 
