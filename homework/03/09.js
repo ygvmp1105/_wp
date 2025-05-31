5e6433272a2326847b2945b4d9215006899dba17
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:12:32 +0800

    Update 09.js

--- homework/03/09.js
+++ homework/03/09.js
@@ -2,3 +2,12 @@
 
 建立一個物件 person = { name: "Alice", age: 25, city: "Taipei" }。
 將 person 的 age 增加 1，並輸出更新後的 person 物件。 */
+let person = {
+    name: "Alice",
+    age: 25,
+    city: "Taipei"
+};
+
+person.age += 1;
+
+console.log(person);
