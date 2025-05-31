7668f01060cf2bbe10c6be5e75e187b6ef120946
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 14:10:09 +0800

    Create 01.js

---
+++ homework/03/01.js
@@ -0,0 +1,10 @@
+/*基本型態
+變數與型態轉換
+
+宣告一個字串變數 num，內容為 "42"，請將其轉換為數字，並加上 8，使其結果為 50。 (字串轉數字可以用 parseInt )
+在 console 輸出結果。*/
+
+let num = "42";
+let result = parseInt(num);
+result = result + 8;
+console.log(result);
