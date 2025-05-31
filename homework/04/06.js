9971aec254aca6e8b47d7ee74b194e1798b14578
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:46:03 +0800

    Update 06.js

--- homework/04/06.js
+++ homework/04/06.js
@@ -1,1 +1,16 @@
-/* */
+/*用函數作為參數來實現自訂過濾
+實作 filterArray(arr, predicate)，回傳符合條件的陣列。
+
+console.log(filterArray([1, 2, 3, 4, 5], n => n % 2 === 0)); 
+// [2, 4] */
+function filterArray(arr, predicate) {
+    let result = [];
+    for (let item of arr) {
+        if (predicate(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], n => n % 2 === 0)); 
