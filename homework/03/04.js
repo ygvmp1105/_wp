9f3f93cd50804dc72552eaf9eb0d4e4128268968
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:06:03 +0800

    Update 04.js

--- homework/03/04.js
+++ homework/03/04.js
@@ -3,4 +3,8 @@
 建立一個陣列 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]。
 過濾出所有的偶數，並輸出新的陣列。*/
 
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+let evenNums = nums.filter(num => num % 2 === 0);
+
+console.log(evenNums);
