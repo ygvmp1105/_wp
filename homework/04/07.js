9750d6af1d0c89ab5224d71ffd72895e43efa52b
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:47:27 +0800

    Update 07.js

--- homework/04/07.js
+++ homework/04/07.js
@@ -1,1 +1,32 @@
-/* */
+/*請寫一個向量物件，要包含加減與內積運算
+實作 class Vector {...}
+
+let a = new Vector([1,2,3])
+let b = new Vector([4,5,6])
+
+console.log(a.add(b).sub(b).dot(b)) */
+class Vector {
+  constructor(components) {
+    this.components = components;
+  }
+
+  add(other) {
+    let result = this.components.map((val, i) => val + other.components[i]);
+    return new Vector(result);
+  }
+
+  sub(other) {
+    let result = this.components.map((val, i) => val - other.components[i]);
+    return new Vector(result);
+  }
+
+  dot(other) {
+    return this.components.reduce((sum, val, i) => sum + val * other.components[i], 0);
+  }
+}
+
+
+let a = new Vector([1, 2, 3]);
+let b = new Vector([4, 5, 6]);
+
+console.log(a.add(b).sub(b).dot(b));
