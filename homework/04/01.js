402f9cc18752817286494a4f3ca431023bbec83f
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:24:26 +0800

    Update 01.js

--- homework/04/01.js
+++ homework/04/01.js
@@ -1,1 +1,21 @@
-/* */
+/* 計算字串中字母出現的次數
+實作 countLetters(str)，輸入一個字串，回傳一個 Map，其鍵為字母，值為該字母出現的次數。
+
+console.log(countLetters("banana")); 
+// Map { 'b' => 1, 'a' => 3, 'n' => 2 } */
+function countLetters(str) {
+    let letterMap = new Map();
+
+    for (let char of str) {
+        if (letterMap.has(char)) {
+            letterMap.set(char, letterMap.get(char) + 1);
+        } else {
+            letterMap.set(char, 1);
+        }
+    }
+
+    return letterMap;
+}
+
+console.log(countLetters("banana")); 
+// Map { 'b' => 1, 'a' => 3, 'n' => 2 }
