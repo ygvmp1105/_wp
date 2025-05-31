96f96a5a853fec2b4f06725b22ec82953701c2ca
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:43:06 +0800

    Update 05.js

--- homework/04/05.js
+++ homework/04/05.js
@@ -1,1 +1,26 @@
-/* */
+/*類別與繼承
+定義一個 Animal 類別，具有 name 屬性與 speak() 方法。
+建立 Dog 類別，繼承 Animal 並覆寫 speak() 方法，使其回傳 "Woof! I am <name>"。
+
+const dog = new Dog("Buddy");
+console.log(dog.speak()); 
+// "Woof! I am Buddy" */
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    return `${this.name} makes a sound.`;
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    return `Woof! I am ${this.name}`;
+  }
+}
+
+
+const dog = new Dog("Buddy");
+console.log(dog.speak());
