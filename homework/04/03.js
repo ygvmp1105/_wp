a8a9af4b6ee3a89fc99abb116d3a70001c2c88f3
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:26:53 +0800

    Update 03.js

--- homework/04/03.js
+++ homework/04/03.js
@@ -1,1 +1,14 @@
-/* */
+/*計算陣列數字總和
+實作 sumArray(arr)，計算數字陣列的總和。
+
+console.log(sumArray([1, 2, 3, 4])); 
+// 10 */
+function sumArray(arr) {
+    let sum = 0;
+    for (let num of arr) {
+        sum += num;
+    }
+    return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4])); 
