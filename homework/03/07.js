060eb3bd5199d393f2b1f5210da9f888a7f35e0f
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:09:55 +0800

    Update 07.js

--- homework/03/07.js
+++ homework/03/07.js
@@ -2,3 +2,11 @@
 
 建立一個陣列 arr = [2, 4, 6, 8, 10]。
 使用 for 迴圈計算陣列元素的總和，並輸出結果。 */
+let arr = [2, 4, 6, 8, 10];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+console.log(sum);
