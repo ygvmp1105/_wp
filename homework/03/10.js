09e4090920b0faef89df5b990063059d90c27884
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-05-31 17:14:18 +0800

    Update 10.js

--- homework/03/10.js
+++ homework/03/10.js
@@ -2,3 +2,12 @@
 
 建立一個物件 scores = { Alice: 85, Bob: 92, Charlie: 78 }。
 使用 for...in 迴圈遍歷 scores 物件，輸出 "Alice 的分數是 85" 這樣的資訊。 */
+let scores = {
+    Alice: 85,
+    Bob: 92,
+    Charlie: 78
+};
+
+for (let name in scores) {
+    console.log(`${name} 的分數是 ${scores[name]}`);
+}
