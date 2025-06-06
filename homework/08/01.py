4edd2415d9ca73121272e87eda7d45d5e61d4d0b
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-06-06 18:09:16 +0800

    Update 01.py

--- homework/08/01.py
+++ homework/08/01.py
@@ -1,1 +1,12 @@
+def find_max(numbers):
+    if not numbers:
+        return None
+    max_num = numbers[0]
+    for num in numbers[1:]:
+        if num > max_num:
+            max_num = num
+    return max_num
 
+print(find_max([3, 5, 1, 9, 2]))
+print(find_max([-10, -5, -3]))
+print(find_max([100]))
