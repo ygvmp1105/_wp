4e2b7f52724e3673e38253129fa7b9268397531b
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2025-06-06 18:41:51 +0800

    Create 10.py

---
+++ homework/08/10.py
@@ -0,0 +1,14 @@
+def summary(data):
+    for student in data:
+        name = student['name']
+        scores = student['scores']
+        total = sum(scores)
+        avg = round(total / len(scores), 1) if scores else 0
+        print(f"{name} 總分: {total}, 平均: {avg}")
+
+students = [
+    {'name': 'Andy', 'scores': [90, 80, 70]},
+    {'name': 'jack', 'scores': [60, 85, 95]},
+    {'name': 'alan', 'scores': [160, 185, 195]},
+]
+summary(students)
